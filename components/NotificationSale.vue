3f332a2b544dae5e24fe680632d11d1ad7d3f766
= <thales.mariano125@gmail.com>
2025-01-22 18:36:29 -0300

    feat: style updates

--- components/NotificationSale.vue
+++ components/NotificationSale.vue
@@ -1,17 +1,15 @@
 <template>
-    <div class="relative w-72 h-12 rounded-xl overflow-hidden">
-        <div class="bg-white/50 w-full h-full blur-lg"></div>
-
-        <div class="absolute left-0 top-0 w-full h-full flex justify-start p-2">
-            <div class="min-w-[2.2rem] w-[2.2rem] h-full bg-white flex justify-center items-center rounded-md">
-                <img class="min-w-5 w-5" src="@/assets/images/blinky_icon_big.png" />
+    <div class="w-80 h-14 bg-white/30 rounded-2xl">
+        <div class="w-full h-full flex justify-start p-3">
+            <div class="min-w-8 w-8 h-full bg-white flex justify-center items-center rounded-md">
+                <img class="min-w-4 w-4" src="@/assets/images/blinky_icon_big.png" />
             </div>
             <div class="w-full flex justify-between ml-3">
                 <div>
-                    <p class="text-xs text-white">Venda Realizada!</p>
-                    <p class="text-xs text-neutral-300">Valor: R$ {{ Math.floor(Math.random() * 999) }},00</p>
+                    <p class="text-[11px] text-white font-poppins font-medium">Venda Realizada!</p>
+                    <p class="text-[11px] text-white font-poppins">Valor: R$ {{ Math.floor(Math.random() * 999) }},00</p>
                 </div>
-                <p class="text-xs text-neutral-300/60">Agora</p>
+                <p class="text-[11px] text-neutral-300/60 font-poppins font-light">Agora</p>
             </div>
         </div>
         
