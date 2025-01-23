9def25cbfdaa66468be2b188f9ae1a62bcb6f4b5
= <thales.mariano125@gmail.com>
2025-01-23 15:12:38 -0300

    feat: remove register button component

--- components/RegisterButton.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <a class="flex gap-3 items-center text-sm font-poppins font-semibold text-black bg-gradient-to-r from-blinkyBlue to-blinkyGreen px-4 py-1 rounded-lg">
-        Cadastre-se
-        <IconsArrowForward width="20" height="20" stroke="#000" />
-    </a>
-</template>
