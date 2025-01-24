807cb48338589abc577cce94a556bb91ce4ecef0
= <thales.mariano125@gmail.com>
2025-01-24 19:04:33 -0300

    feat: srr desactived

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  ssr: true,
+  ssr: false,
   modules: ['@nuxtjs/tailwindcss']
 })
