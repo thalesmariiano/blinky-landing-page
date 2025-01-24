61f78df975e43d91716b02a6e28fd398888ba36f
= <thales.mariano125@gmail.com>
2025-01-24 13:44:35 -0300

    feat: add responsivity to ProtagonistaSection

--- components/ProtagonistaSection.vue
+++ components/ProtagonistaSection.vue
@@ -13,9 +13,9 @@
 </script>
 
 <template>
-     <section @mousemove="parallax" class="relative -skew-y-[4deg] bg-[#070707] overflow-hidden">
-        <img class="w-full max-w-5xl absolute -top-80 -left-96 opacity-5 rotate-12 skew-y-[4deg]" src="@/assets/images/blinky_icon_white.png" />
-        <img class="w-full max-w-5xl absolute -bottom-80 -right-96 opacity-5 skew-y-[4deg]" src="@/assets/images/blinky_icon_white.png" />
+     <section @mousemove="parallax" class="relative -skew-y-[4deg] bg-[#070707] overflow-hidden px-2">
+        <img class="w-full max-w-5xl absolute -top-32 sm:-top-80 -left-64 sm:-left-96 opacity-5 rotate-12 skew-y-[4deg]" src="@/assets/images/blinky_icon_white.png" />
+        <img class="w-full max-w-5xl absolute -bottom-32 sm:-bottom-80 -right-64 sm:-right-96 opacity-5 skew-y-[4deg]" src="@/assets/images/blinky_icon_white.png" />
         <img class="w-full max-w-5xl absolute -top-80 mix-blend-screen skew-y-[4deg]" src="@/assets/images/light_blur_division.png" />
         <img class="w-full max-w-5xl absolute -bottom-48 -right-36 mix-blend-screen skew-y-[4deg]" src="@/assets/images/light_blur_division.png" />
 
@@ -23,23 +23,23 @@
             <img class="w-14 absolute top-28 drop-shadow-blinkyIcon" src="@/assets/images/blinky_icon_big.png" />
 
             <!-- parallax icons -->
-            <img ref="protection_icon" class="w-20 absolute top-32 -right-20" src="@/assets/images/protection_dark_icon.png" />
-            <img ref="speed_icon" class="w-20 absolute top-72 -left-16" src="@/assets/images/speed_dark_icon.png" />
-            <img ref="security_icon" class="w-20 absolute bottom-24 left-72" src="@/assets/images/security_dark_icon.png" />
+            <img ref="protection_icon" class="w-20 absolute top-24 sm:top-32 -right-0 sm:-right-20" src="@/assets/images/protection_dark_icon.png" />
+            <img ref="speed_icon" class="w-20 absolute top-64 sm:top-72 -left-0 sm:-left-16" src="@/assets/images/speed_dark_icon.png" />
+            <img ref="security_icon" class="w-20 absolute bottom-24 left-64 sm:left-72" src="@/assets/images/security_dark_icon.png" />
             <!-- parallax icons -->
 
             <div class="flex gap-10 flex-col">
-                <h1 class="font-poppins font-semibold text-5xl text-center text-white">
+                <h1 class="font-poppins font-semibold text-4xl md:text-5xl text-center text-white">
                     <span class="text-blinkyOceanGreen">Seja Protagonista</span> <br>
                     o mundo digital <br>
                     com a <span class="relative back-stroke text-shadow">Blinky!</span>
                 </h1>
-                <p class="font-poppins font-light text-white text-center text-lg">
+                <p class="font-poppins font-light text-white text-center md:text-lg">
                     Como produtor, coprodutor ou afiliado, encontre na <br>
                     nossa plataforma o suporte perfeito para transformar <br>
                     ideias em lucro.
                 </p>
-                <a class="hoverAnimation w-full flex gap-3 justify-center items-center font-poppins font-semibold text-lg text-black bg-gradient-to-r from-blinkyBlue to-blinkyGreen px-4 py-1.5 rounded-lg">
+                <a class="hoverAnimation w-full flex gap-3 justify-center items-center font-poppins font-semibold md:text-lg text-black bg-gradient-to-r from-blinkyBlue to-blinkyGreen px-4 py-1.5 rounded-lg">
                         Cadastre-se
                     <IconsArrowForward width="20" height="20" stroke="#000" />
                 </a>
