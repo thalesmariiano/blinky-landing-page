ede7f1cb14db3ad9a7aa7bfc52577fa6ffd2a908
= <thales.mariano125@gmail.com>
2025-01-24 15:26:57 -0300

    feat: notificaton style adjust

--- components/ManNotification.vue
+++ components/ManNotification.vue
@@ -4,15 +4,15 @@
         <img class="w-96 md:w-[28rem] md:absolute -left-32 -top-36 mix-blend-screen" src="@/assets/images/light_blur_front.png" />
         
         
-        <div class="absolute top-0 right-0 notificationSale opacity-0 -z-5" style="animation-delay: 1s">
+        <div class="absolute top-0 right-0 notificationSale opacity-80 -z-5" style="animation-delay: 1s">
             <NotificationSale />
         </div>
 
-        <div class="absolute top-16 right-0 notificationSale opacity-0 -z-5" style="animation-delay: 2s">
+        <div class="absolute top-16 right-0 notificationSale opacity-80 -z-5" style="animation-delay: 2s">
             <NotificationSale />
         </div>
 
-        <div class="absolute top-32 right-0 notificationSale opacity-0 -z-5" style="animation-delay: 3s">
+        <div class="absolute top-32 right-0 notificationSale opacity-80 -z-5" style="animation-delay: 3s">
             <NotificationSale />
         </div>
     </div>
@@ -28,9 +28,9 @@
             opacity: 0;
         }
         50% {
-            opacity: 1;
+            opacity: 0.8;
             transform:translateY(-50px);
-            filter: drop-shadow(0px 10px 10px rgba(0,0,0,.5));
+            filter: drop-shadow(0px 10px 10px rgba(0,0,0,.3));
         }
         100% {
             opacity: 0;
