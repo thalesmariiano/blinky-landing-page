d6931fc75a16fab6df27349a18222e0a9628b2a4
= <thales.mariano125@gmail.com>
2025-01-24 17:33:52 -0300

    fix: sidebar update

--- components/SideMenu.vue
+++ components/SideMenu.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-80 h-screen fixed top-0 right-0 bg-black/20 backdrop-blur-md md:hidden p-5">
+    <div class="w-full h-screen fixed top-0 right-0 bg-[#083027] md:hidden p-5">
         <div class="w-full flex justify-end">
             <button @click="$emit('close')">
                 <IconsClose widht="44" height="44" fill="#fff" />
@@ -19,8 +19,8 @@
                 <li class="text-center">
                     <a href="#contato" class="font-poppins font-light text-white text-lg">Contato</a>
                 </li>
-                <li>
-                    <a class="hoverAnimation flex gap-3 justify-center items-center text-sm font-poppins font-semibold text-black bg-gradient-to-r from-blinkyBlue to-blinkyGreen px-4 py-1.5 rounded-lg">
+                <li class="text-center">
+                    <a class="w-72 hoverAnimation mx-auto flex gap-3 justify-center items-center text-sm font-poppins font-semibold text-black bg-gradient-to-r from-blinkyBlue to-blinkyGreen px-4 py-1.5 rounded-lg">
                         Cadastre-se
                         <IconsArrowForward width="20" height="20" stroke="#000" />
                     </a>
