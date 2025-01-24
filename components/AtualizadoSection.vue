fecefc45ac02576b041a8344d9a9a128c071e0da
= <thales.mariano125@gmail.com>
2025-01-24 16:09:20 -0300

    feat: add responsivity to AtualizadoSection

--- components/AtualizadoSection.vue
+++ components/AtualizadoSection.vue
@@ -1,65 +1,59 @@
-<script setup lang="ts">
-import ManNotification from './ManNotification.vue';
-
-</script>
-
 <template>
-        <section class="w-full flex gap-10 flex-col justify-center items-center pt-32 pb-5 px-2 bg-black border-t-2 border-neutral-300/10">
-            <div class="w-full max-w-5xl h-[27rem] lg:h-[33rem] flex flex-col md:flex-row justify-center items-center">
-                <div class="relative w-full max-w-md md:max-w-xl h-full flex flex-col justify-center items-center p-5 bg-gradient-to-tr from-blinkyLittleDark to-blinkyDark/80 rounded-r-3xl md:rounded-r-none rounded-l-3xl">
-                    <img class="w-9 lg:w-11 absolute top-3 lg:top-6 mr-24 lg:mr-0 drop-shadow-blinkyIcon" src="@/assets/images/blinky_icon_big.png" />
+    <section class="w-full space-y-10 pt-32 pb-24 px-4 bg-black border-t-2 border-neutral-300/10">
+        <div class="w-full flex flex-col md:flex-row justify-center items-center">
+                
+            <div class="relative w-full max-w-lg lg:max-w-xl h-[25rem] lg:h-[33rem] flex flex-col justify-center items-center p-2 sm:p-5 bg-gradient-to-tr from-blinkyLittleDark to-blinkyDark/80 rounded-t-3xl md:rounded-tr-none md:rounded-l-3xl">
+                
+                <img class="w-9 lg:w-11 absolute top-3 lg:top-6 md:mr-24 lg:mr-0 drop-shadow-blinkyIcon" src="@/assets/images/blinky_icon_big.png" />
 
-                    <div class="space-y-7 text-center md:text-left">
-                        <h1 class="font-poppins font-semibold text-2xl lg:text-3xl text-white">
-                            <span class="text-blinkyGreen">Mantenha-se atualizado</span> <br>
-                            sobre tudo o que ocorre em <br>
-                            sua operação através do <br>
-                            nosso <span class="text-blinkyGreen italic">aplicativo exclusivo.</span>
-                        </h1>
-                        <p class="font-poppins font-light text-white text-sm lg:text-base">
-                            Com uma tecnologia incomparável, <span class="font-semibold italic">a Blinky é <br>
-                            a solução definitiva para o processamento de <br>
-                            pagamentos</span>, proporcionando uma experiência <br>
-                            execepcional para os seus clientes e facilitando <br>
-                            cada transação e saque pelo aplicativo.
-                        </p>
-                        <div class="flex gap-4 justify-center items-center">
-                            <p class="font-poppins font-semibold italic text-white">Baixe o aplicativo: </p>
-                            <a><img class="w-28 hover:scale-105 transition-all" src="@/assets/images/app_store.png"></a>
-                            <a><img class="w-28 hover:scale-105 transition-all" src="@/assets/images/play_store.png"></a>
-                        </div>
+                <div class="space-y-7 text-center md:text-left">
+                    <h1 class="font-poppins font-semibold text-lg sm:text-xl lg:text-3xl text-white">
+                        <span class="text-blinkyGreen">Mantenha-se atualizado</span> <br>
+                        sobre tudo o que ocorre em <br>
+                        sua operação através do <br>
+                        nosso <span class="text-blinkyGreen italic">aplicativo exclusivo.</span>
+                    </h1>
+                    <p class="font-poppins font-light text-white text-xs sm:text-sm lg:text-base">
+                        Com uma tecnologia incomparável, <span class="font-semibold italic">a Blinky é <br>
+                        a solução definitiva para o processamento de <br>
+                        pagamentos</span>, proporcionando uma experiência <br>
+                        execepcional para os seus clientes e facilitando <br>
+                        cada transação e saque pelo aplicativo.
+                    </p>
+                    <div class="flex gap-4 justify-center items-center">
+                        <p class="font-poppins font-semibold italic text-white">Baixe o aplicativo: </p>
+                        <a><img class="w-28 hover:scale-105 transition-all" src="@/assets/images/app_store.png"></a>
+                        <a><img class="w-28 hover:scale-105 transition-all" src="@/assets/images/play_store.png"></a>
                     </div>
+                </div>
                     
-                </div>
-                <div class="relative w-full max-w-md h-full md:bg-gradient-to-br from-blinkyBlue to-blinkyGreen rounded-r-3xl">
-                    <ManNotification />
-                </div>
+            </div>
+            <ManNotification />
+        </div>
+
+        <div class="w-full max-w-2xl mx-auto bg-blinkyLittleDark border-2 border-neutral-700 px-3 sm:px-10 md:px-20 py-5 rounded-3xl">
+            <div class="flex gap-5 items-center">
+                <label class="flex gap-4 items-center group">
+                    <input type="checkbox" name="checkbox" class="hidden" checked disabled />
+                    <div class="w-4 h-4 border-2 border-blinkyGreen rounded transition-all">
+                        <div class="w-full h-full bg-blinkyGreen check-path hidden group-has-[:checked]:block"></div>
+                    </div>  
+                </label>
+                <p class="font-poppins italic text-white text-xs sm:text-base">Sem improviso para visualizar suas notificações de venda.</p>
             </div>
 
-            <div class="w-full max-w-2xl bg-blinkyLittleDark border-2 border-neutral-700 px-20 py-5 rounded-3xl">
-                <div class="flex gap-5 items-center">
-                    <label class="flex gap-4 items-center group">
-                        <input type="checkbox" name="checkbox" class="hidden" checked disabled />
-                        <div class="w-4 h-4 border-2 border-blinkyGreen rounded transition-all">
-                            <div class="w-full h-full bg-blinkyGreen check-path hidden group-has-[:checked]:block"></div>
-                        </div>  
-                    </label>
-                    <p class="font-poppins italic text-white">Sem improviso para visualizar suas notificações de venda.</p>
-                </div>
-
-                <div class="flex gap-5 items-center">
-                    <label class="flex gap-4 items-center group">
-                        <input type="checkbox" name="checkbox" class="hidden" checked disabled />
-                        <div class="w-4 h-4 border-2 border-blinkyGreen rounded transition-all">
-                            <div class="w-full h-full bg-blinkyGreen check-path hidden group-has-[:checked]:block"></div>
-                        </div>
-                    </label>
-                    <p class="font-poppins italic text-white">Sem dificuldade para realizar o saque pelo celular.</p>
-                </div>
+            <div class="flex gap-5 items-center">
+                <label class="flex gap-4 items-center group">
+                    <input type="checkbox" name="checkbox" class="hidden" checked disabled />
+                    <div class="w-4 h-4 border-2 border-blinkyGreen rounded transition-all">
+                        <div class="w-full h-full bg-blinkyGreen check-path hidden group-has-[:checked]:block"></div>
+                    </div>
+                </label>
+                <p class="font-poppins italic text-white text-xs sm:text-base">Sem dificuldade para realizar o saque pelo celular.</p>
+            </div>   
                 
-                
-            </div>
-        </section>
+        </div>
+    </section>
 </template>
 
 <style scoped>
--- components/ManNotification.vue
+++ components/ManNotification.vue
@@ -1,19 +1,23 @@
 <template>
-    <div>
-        <img class="w-[22rem] lg:w-[26rem] absolute -left-20 bottom-0 z-10" src="@/assets/images/man_on_phone.png" />
-        <img class="w-96 md:w-[28rem] md:absolute -left-32 -top-36 mix-blend-screen" src="@/assets/images/light_blur_front.png" />
-        
-        
-        <div class="absolute top-0 right-0 notificationSale opacity-80 -z-5" style="animation-delay: 1s">
-            <NotificationSale />
+    <div class="relative w-full max-w-lg md:max-w-sm lg:max-w-md h-[25rem] lg:h-[33rem] bg-gradient-to-br from-blinkyBlue to-blinkyGreen rounded-b-3xl md:rounded-bl-none md:rounded-r-3xl">
+        <div class="absolute lg:-left-20 bottom-0 z-10">
+            <img class="w-72 sm:w-80 lg:w-[26rem]" src="@/assets/images/man_on_phone.png" />
         </div>
 
-        <div class="absolute top-16 right-0 notificationSale opacity-80 -z-5" style="animation-delay: 2s">
-            <NotificationSale />
-        </div>
+        <img class="w-[28rem] absolute -left-32 -top-44 mix-blend-screen z-20" src="@/assets/images/light_blur_front.png" />
+        
+        <div class="relative -z-5 mt-10 scale-75 sm:scale-100">
+            <div class="absolute top-0 -right-12 sm:-right-3 lg:right-0 notificationSale opacity-80" style="animation-delay: 1s">
+                <NotificationSale />
+            </div>
 
-        <div class="absolute top-32 right-0 notificationSale opacity-80 -z-5" style="animation-delay: 3s">
-            <NotificationSale />
+            <div class="absolute top-16 -right-12 sm:-right-3 lg:right-0 notificationSale opacity-80" style="animation-delay: 2s">
+                <NotificationSale />
+            </div>
+
+            <div class="absolute top-32 -right-12 sm:-right-3 lg:right-0 notificationSale opacity-80" style="animation-delay: 3s">
+                <NotificationSale />
+            </div>
         </div>
     </div>
 </template>
