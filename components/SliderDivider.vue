9cee2daf3cbcd8c7343f0a87ee8c878c6dbfcc61
= <thales.mariano125@gmail.com>
2025-01-24 19:05:12 -0300

    feat: add slide effect to SliderDivider

--- components/SliderDivider.vue
+++ components/SliderDivider.vue
@@ -1,10 +1,50 @@
+<script setup lang="ts">
+    import Splide from '@splidejs/splide'
+	import { AutoScroll } from '@splidejs/splide-extension-auto-scroll';
+
+    const splide = useTemplateRef('splide')
+
+	onMounted(() => {
+		new Splide(splide.value, {
+			type: 'loop',
+			perPage: 3,
+			arrows: false,
+			drag: false,
+			pagination: false,
+            fixedWidth: '100%',
+			autoScroll: {
+				speed: 1.5
+			}
+		}).mount({ AutoScroll })		
+	})
+</script>
+
 <template>
-    <div class="glowEffect absolute -top-12 left-0 w-full h-14 sm:h-[4.5rem] flex gap-10 md:gap-20 justify-center items-center bg-gradient-to-r from-blinkyBlue to-blinkyGreen">
-        <p class="font-poppins font-semibold text-xl md:text-2xl whitespace-nowrap">As menores taxas do mercado</p>
-        <img class="w-8" src="@/assets/images/blinky_icon_black.png" />
-        <p class="font-poppins font-semibold text-xl md:text-2xl whitespace-nowrap">Saque instantâneo</p>
-        <img class="w-8" src="@/assets/images/blinky_icon_black.png" />
-        <p class="font-poppins font-semibold text-xl md:text-2xl whitespace-nowrap">Saques via Criptomoeda (USDT)</p>
+    <div class="glowEffect absolute -top-12 left-0 w-full h-14 sm:h-[4.5rem] flex items-center bg-gradient-to-r from-blinkyBlue to-blinkyGreen">
+        <section ref="splide" class="splide">
+            <div class="splide__track">
+                <ul class="splide__list">
+                    <li class="splide__slide">
+                        <p class="font-poppins font-semibold text-xl md:text-2xl whitespace-nowrap">As menores taxas do mercado</p>
+                    </li>
+                    <li class="splide__slide">
+                        <img class="w-8" src="@/assets/images/blinky_icon_black.png" />
+                    </li>
+                    <li class="splide__slide">
+                        <p class="font-poppins font-semibold text-xl md:text-2xl whitespace-nowrap">Saque instantâneo</p>
+                    </li>
+                    <li class="splide__slide">
+                        <img class="w-8" src="@/assets/images/blinky_icon_black.png" />
+                    </li>
+                    <li class="splide__slide">
+                        <p class="font-poppins font-semibold text-xl md:text-2xl whitespace-nowrap">Saques via Criptomoeda (USDT)</p>
+                    </li>
+                    <li class="splide__slide">
+                        <img class="w-8" src="@/assets/images/blinky_icon_black.png" />
+                    </li>
+                </ul>
+            </div>
+        </section>
     </div>
 </template>
 
