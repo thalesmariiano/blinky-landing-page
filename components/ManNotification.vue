280f8a1e8365afe13b3a9dbb23cb201a8a40897e
= <thales.mariano125@gmail.com>
2025-01-24 00:12:29 -0300

    feat: adjust notifications animation

--- components/ManNotification.vue
+++ components/ManNotification.vue
@@ -4,15 +4,15 @@
         <img class="w-[28rem] absolute -left-32 -top-36 mix-blend-screen" src="@/assets/images/light_blur_front.png" />
         
         
-        <div class="absolute top-0 right-0 notificationSale opacity-0 -z-5" style="animation-delay: 5s">
+        <div class="absolute top-0 right-0 notificationSale opacity-0 -z-5" style="animation-delay: 1s">
             <NotificationSale />
         </div>
 
-        <div class="absolute top-24 right-0 notificationSale opacity-0 -z-5" style="animation-delay: 3s">
+        <div class="absolute top-16 right-0 notificationSale opacity-0 -z-5" style="animation-delay: 2s">
             <NotificationSale />
         </div>
 
-        <div class="absolute top-48 right-0 notificationSale opacity-0 -z-5" style="animation-delay: 1s">
+        <div class="absolute top-32 right-0 notificationSale opacity-0 -z-5" style="animation-delay: 3s">
             <NotificationSale />
         </div>
     </div>
@@ -20,7 +20,7 @@
 
 <style scoped>
     .notificationSale {
-        animation: swipingUp 5s infinite;
+        animation: swipingUp 4s infinite;
     }
 
     @keyframes swipingUp {
@@ -30,10 +30,11 @@
         50% {
             opacity: 1;
             transform:translateY(-50px);
+            filter: drop-shadow(0px 10px 10px rgba(0,0,0,.5));
         }
         100% {
             opacity: 0;
-            transform:translateY(-100px);
+            transform:translateY(-60px);
         }
 
     }
