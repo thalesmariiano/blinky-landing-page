51edb7acc56aa04dad9b9eef4e0403e82d0cc308
= <thales.mariano125@gmail.com>
2025-01-26 22:37:45 -0300

    fix: ContatoSection text adjust

--- components/ContatoSection.vue
+++ components/ContatoSection.vue
@@ -5,20 +5,20 @@
         <div class="relative w-full max-w-md sm:max-w-xl h-56 sm:h-72 bg-[#e6e6e6] rounded-t-3xl lg:hidden">
             <img class="absolute bottom-0" src="@/assets/images/woman_mobile.png" />
         </div>
-        <div class="relative w-full max-w-md sm:max-w-xl lg:max-w-5xl lg:h-80 bg-blinkyLittleBege flex items-center rounded-b-3xl lg:rounded-3xl p-5 sm:p-14">
+        <div class="relative w-full max-w-md sm:max-w-xl lg:max-w-5xl lg:h-80 bg-blinkyLittleBege flex items-center rounded-b-3xl lg:rounded-3xl px-5 py-10 sm:p-14">
             <div class="w-full lg:w-fit flex gap-5 flex-col justify-center items-center lg:items-start">
-                <h3 class="font-poppins font-semibold text-2xl sm:text-3xl text-center lg:text-left">
-                    Conecte-se e potencialize <br>
+                <h3 class="w-full max-w-md font-poppins font-semibold text-2xl sm:text-3xl text-center lg:text-left">
+                    Conecte-se e potencialize
                     seus negócios conosco!
                 </h3>
 
-                <p class="w-[18rem] sm:w-full font-poppins font-light text-sm sm:text-base text-center lg:text-left">
-                    Descubra como a Blinky pode transformar a maneira <br>
-                    como você conduz seus negócios. Conecte-se conosco <br>
+                <p class="w-full max-w-md font-poppins font-light text-sm sm:text-base text-center lg:text-left">
+                    Descubra como a Blinky pode transformar a maneira
+                    como você conduz seus negócios. Conecte-se conosco
                     hoje e abra as portas para um futuro de prosperidade.
                 </p>
 
-                <a class="hoverAnimation flex gap-3 items-center font-poppins text-black font-semibold bg-gradient-to-r from-blinkyBlue to-blinkyGreen px-12 py-1.5 rounded-lg">
+                <a class="hoverAnimation flex gap-3 mt-2 lg:mt-0 items-center font-poppins text-black font-semibold bg-gradient-to-r from-blinkyBlue to-blinkyGreen px-12 py-1.5 rounded-lg">
                     Fale Conosco
                     <IconsArrowForward width="20" height="20" stroke="#000" />
                 </a>
