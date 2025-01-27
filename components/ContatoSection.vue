de16abfc9eb316eacf08501c500e9826548126c1
= <thales.mariano125@gmail.com>
2025-01-26 22:04:38 -0300

    feat: add responsivity to ContatoSection

--- components/ContatoSection.vue
+++ components/ContatoSection.vue
@@ -1,19 +1,18 @@
 <template>
-    <section class="relative w-full flex gap-5 flex-col justify-center items-center bg-[#070707] py-32 overflow-hidden px-2">
+    <section class="relative w-full flex flex-col justify-center items-center bg-[#070707] py-32 overflow-hidden px-2">
         <img class="w-full max-w-6xl absolute -left-48 -top-96 mix-blend-screen" src="@/assets/images/light_blur_division.png" />
 
-        <div class="lg:hidden relative w-full max-w-5xl h-80 bg-blinkyLittleBege flex justify-center rounded-3xl">
-            <img class="absolute bottom-0 w-full min-w-[48.5rem] max-w-[48.5rem]" src="@/assets/images/woman_with_icon.png" />
+        <div class="relative w-full max-w-md sm:max-w-xl h-56 sm:h-72 bg-[#e6e6e6] rounded-t-3xl lg:hidden">
+            <img class="absolute bottom-0" src="@/assets/images/woman_mobile.png" />
         </div>
-
-        <div class="relative w-full max-w-5xl h-80 bg-blinkyLittleBege flex items-center rounded-3xl p-2 sm:p-14">
+        <div class="relative w-full max-w-md sm:max-w-xl lg:max-w-5xl lg:h-80 bg-blinkyLittleBege flex items-center rounded-b-3xl lg:rounded-3xl p-5 sm:p-14">
             <div class="w-full lg:w-fit flex gap-5 flex-col justify-center items-center lg:items-start">
-                <h3 class="font-poppins font-semibold text-xl sm:text-3xl text-center lg:text-left">
+                <h3 class="font-poppins font-semibold text-2xl sm:text-3xl text-center lg:text-left">
                     Conecte-se e potencialize <br>
                     seus negócios conosco!
                 </h3>
 
-                <p class="font-poppins font-light text-[11px] sm:text-base text-center lg:text-left">
+                <p class="font-poppins font-light text-sm sm:text-base text-center lg:text-left">
                     Descubra como a Blinky pode transformar a maneira <br>
                     como você conduz seus negócios. Conecte-se conosco <br>
                     hoje e abra as portas para um futuro de prosperidade.
