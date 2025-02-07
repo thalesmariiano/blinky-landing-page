caca3072479f2433fb37079822bd09ee8421dab0
= <thales.mariano125@gmail.com>
2025-02-07 17:06:07 -0300

    feat: ssr actived

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  ssr: false,
+  ssr: true,
   modules: ['@nuxtjs/tailwindcss'],
   app: {
     head: {
