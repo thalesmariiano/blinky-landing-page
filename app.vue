5e14bc6f9fa5b74b767296cf354737e2b2c3a048
= <thales.mariano125@gmail.com>
2025-02-07 15:52:33 -0300

    feat: useHead and useSEOMeta removed

--- app.vue
+++ app.vue
@@ -1,23 +1,3 @@
-<script setup lang="ts">
-    useHead({
-        title: "Blinky",
-        meta: [
-            {
-                name: 'description',
-                content: 'Simplifique, Conquiste e Desfrute. Transforme cada venda em um passo a passo para o topo e venda mais pagando menos.'
-            }
-        ],
-    })
-    useSeoMeta({
-        title: 'Blinky',
-        ogTitle: 'Blinky',
-        ogType: 'website',
-        ogUrl: 'https://blinkybr.com',
-        description: 'Simplifique, Conquiste e Desfrute. Transforme cada venda em um passo a passo para o topo e venda mais pagando menos.',
-        ogDescription: 'Simplifique, Conquiste e Desfrute. Transforme cada venda em um passo a passo para o topo e venda mais pagando menos.'
-    })
-</script>
-
 <template>
     <div class="w-full bg-black overflow-x-hidden">
         <AppHeader />
