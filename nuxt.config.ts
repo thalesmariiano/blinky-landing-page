5dcb31ac4574ddf1dacd7617c0c7c8ca42333f5f
= <thales.mariano125@gmail.com>
2025-02-07 15:54:00 -0300

    feat: add non-reactive head content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,38 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   ssr: false,
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: "Blinky",
+      meta: [
+        {
+          property: "og:title",
+          content: "Blinky"
+        },
+        {
+          property: "og:description",
+          content: "Simplifique, Conquiste e Desfrute. Transforme cada venda em um passo a passo para o topo e venda mais pagando menos.'"
+        },
+        {
+          name: "description",
+          content: "Simplifique, Conquiste e Desfrute. Transforme cada venda em um passo a passo para o topo e venda mais pagando menos."
+        },
+        {
+          property: "og:url",
+          content: "https://blinkybr.com"
+        },
+        {
+          property: "og:type",
+          content: "website"
+        },
+        {
+          name: "keywords",
+          content: "Blinky, blink, As menores taxas do mercado, e-commerce, digital, pagamento, gateway, afiliado"
+        }
+      ]
+    }
+  }
 })
